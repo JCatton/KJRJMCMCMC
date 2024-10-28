dc4783bb1b7839ebf5ba64e884325583cd140887
JonteC <106551147+JCatton@users.noreply.github.com.>
2024-10-28 18:11:06 +0000

    Fixed decorator to correctly handle function returns. Made timing print in a single line and at high enough resolution for quick functions.

--- sim/Decorators.py
+++ sim/Decorators.py
@@ -1,15 +1,15 @@
-from time import perf_counter
+from time import perf_counter_ns
 from functools import wraps
+
+
 def TimeMeasure(func):
     '''Measure performance of a function'''
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f'{"-"*40}')
-        start_time = perf_counter()
-        func(*args, **kwargs)
-        finish_time = perf_counter()
-        print(f'Function: {func.__name__}')
-        print(f'Time elapsed in seconds: {finish_time - start_time:.2f}')
-        print(f'{"-"*40}')
+        start_time = perf_counter_ns()
+        func_return = func(*args, **kwargs)
+        finish_time = perf_counter_ns()
+        print(f'Function: {func.__name__} took {(finish_time - start_time)*1e-6:.2f} ms')
+        return func_return
     return wrapper
