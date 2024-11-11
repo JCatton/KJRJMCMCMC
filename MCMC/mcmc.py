cf117dce9fe104f9836017e7dc3b5295f46d0f72
JonteC <106551147+JCatton@users.noreply.github.com.>
2024-11-11 18:02:10 +0000

    Fixed the implementation detail for how out of bound proposals are now treated. Also fixed correct final iteration number (negligible but existent impact on long chains)

--- MCMC/mcmc.py
+++ MCMC/mcmc.py
@@ -65,7 +65,9 @@ class MCMC:
 
         # MCMC-Inputs
         self.raw_data: ndarray = raw_data
-        self.param_bounds: List[Tuple[float, float]] = param_bounds
+        self.param_bounds = np.array(param_bounds)
+        self.lower_bounds = self.param_bounds[:, 0]
+        self.upper_bounds = self.param_bounds[:, 1]
         self.proposal_std: ndarray = proposal_std
         self.likelihood_func: Callable = likelihood_func
 
@@ -130,6 +132,19 @@ class MCMC:
                 f"{data_folder} is not a valid saved state of {cls.__name__}"
             )
 
+    def proposal_within_bounds(self, proposals):
+        """
+        Parameters:
+        - proposals: np.ndarray of shape (sim_number, max_cpu_nodes, num_params)
+          The array of proposed parameter values.
+        Returns:
+        - proposal_bools: np.ndarray of shape (sim_number, max_cpu_nodes)
+          Boolean array indicating whether each proposal is within bounds.
+        """
+        is_within_bounds = (proposals >= self.lower_bounds) & (proposals <= self.upper_bounds)
+        proposal_bools = is_within_bounds.all(axis=2)
+        return proposal_bools
+
     def metropolis_hastings(
         self,
         num_of_new_iterations: int,
@@ -157,7 +172,7 @@ class MCMC:
             initial=self.iteration_num, total=max_iteration_number - 1, desc="MCMC Run "
         )
 
-        remaining_iter = num_of_new_iterations - 1
+        remaining_iter = num_of_new_iterations
         while self.iteration_num < (max_iteration_number - 1):
             current_params = self.chain[prev_iter]
             current_likelihood = self.likelihood_chain[prev_iter]
@@ -166,6 +181,12 @@ class MCMC:
                 0, self.proposal_std, size=(self.sim_number, *self.chain[0].shape)
             )
 
+
+
+            proposal_within_bounds = self.proposal_within_bounds(proposals)
+
+            # Keep clipping as easiest solution that works with multiprocessing and
+            # negligible run cost
             for j, (lower, upper) in enumerate(self.param_bounds):
                 proposals[:, :, j] = np.clip(proposals[:, :, j], lower, upper)
 
@@ -189,7 +210,7 @@ class MCMC:
                 prev_iter += 1
                 pbar.update(1)
                 remaining_iter -= 1
-                if np.random.rand() < acceptance_probs[s]:
+                if proposal_within_bounds[s] and (np.random.rand() < acceptance_probs[s]):
                     self.acceptance_num += 1
                     self.chain[prev_iter] = proposals[s]
                     self.likelihood_chain[prev_iter] = proposal_likelihoods[s]
@@ -213,8 +234,8 @@ class MCMC:
                 )
             )
             self.sim_number = min(self.sim_number, remaining_iter)
-            self.chain[self.iteration_num] = current_params
-            self.likelihood_chain[self.iteration_num] = current_likelihood
+            # self.chain[self.iteration_num] = current_params
+            # self.likelihood_chain[self.iteration_num] = current_likelihood
 
         print(f"{acceptance_rate=}")
         pbar.close()
