d4a6e112e7ae18e604717de185c3e6dd9a895815
kaimul19 <kaimulcock19@gmail.com>
2024-11-13 03:17:32 +0000

    Added plots to visualise difference of final chain

--- MCMC/main.py
+++ MCMC/main.py
@@ -128,7 +128,22 @@ def main():
     )
     mcmc.metropolis_hastings(num_iterations)
     mcmc.chain_to_plot_and_estimate(true_vals)
-    mcmc.corner_plot(true_vals)
+    mcmc.corner_plot(true_vals, burn_in_index=30_000)
+
+    plt.plot(
+        times,
+        flux_data_from_params(stellar_params, mcmc.chain[-1], times, analytical_bool=True)
+        - flux_data_from_params(stellar_params, np.array([true_vals]), times, analytical_bool=True),
+            )
+    plt.show()
+
+    plt.plot(times,
+        flux_data_from_params(stellar_params, mcmc.chain[-1], times, analytical_bool=True), label = "Estimated")
+    plt.plot(times,
+        flux_data_from_params(stellar_params, np.array([true_vals]), times, analytical_bool=True), label = "True")
+    plt.legend()
+    plt.show
+    print(mcmc.acceptance_num)
 
 
 if __name__ == "__main__":
