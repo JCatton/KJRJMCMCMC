c11fdc26b227882254acde908943fd559a84052c
kaimul19 <kaimulcock19@gmail.com>
2024-11-14 04:06:49 +0000

    Planet Params now go vertically

--- MCMC/mcmc.py
+++ MCMC/mcmc.py
@@ -284,8 +284,8 @@ class MCMC:
                     f"Estimated {name}: {np.mean(param_samples):.3e}",
                     f", true {name}: {true_vals[body, i]}" if true_vals is not None else None,
                 )
-                axs[body, i].plot(x, param_samples, label=name)
-                axs[body, i].set_ylabel(f"{name}")
+                axs[i, body].plot(x, param_samples, label=name)
+                axs[i, body].set_ylabel(f"{name}")
 
         plt.tight_layout()
         plt.show()
