2a677190daac1ff3b8fa324eb75e625d0cd0506b
JonteC <106551147+JCatton@users.noreply.github.com.>
2024-11-28 00:02:25 +0000

    Priors class updated to get density functions for comparing priors of parameters

--- MCMC/priors.py
+++ MCMC/priors.py
@@ -8,27 +8,56 @@ def uniform_transform(lower_bound: float, upper_bound: float, x: float) -> float
     domain = upper_bound - lower_bound
     return lower_bound + x * domain
 
+def uniform_density(lower_bound: float, upper_bound: float, x: float) -> float:
+    domain = upper_bound - lower_bound
+    return 1/domain
+
 def gaussian_truncated_transform(lower_bound: float, upper_bound: float, mean: float, std: float, x: float) -> float:
     return scipy.stats.truncnorm.ppf(x, lower_bound, upper_bound, loc=mean, scale=std)
+
+def gaussian_truncated_density(lower_bound: float, upper_bound: float, mean: float, std: float, x: float) -> float:
+    return scipy.stats.truncnorm.pdf(x, lower_bound, upper_bound, loc=mean, scale=std)
 
 def dirac_delta_transform(fixed_val: float, x: float) -> float:
     return fixed_val
 
+def dirac_delta_density(fixed_val: float, x: float) -> float:
+    return fixed_val == x
+
 
 class Priors:
-    def __init__(self, transform_func: Callable, config: dict[str, float]):
+    def __init__(self, prior_func: Callable, transform_func: Callable, config: dict[str, float]):
+        self.prior_func = prior_func
         self.transform_func = transform_func
         self.config = config
 
-    def get
-
     @classmethod
-    def get_truncated_gaussian(lower_bound: float, upper_bound: float, mean: float, std: float) -> Self:
+    def get_truncated_gaussian_prior(cls, lower_bound: float, upper_bound: float, mean: float, std: float) -> Self:
         config = {
                 "lower_bound":lower_bound,
                 "upper_bound": upper_bound,
                 "mean": mean,
                 "std": std,
         }
-        transform_func =
-        return Priors()
+        transform_func = lambda x, config=config: gaussian_truncated_transform(**config, x=x)
+        prior_func = lambda x, config=config: gaussian_truncated_density(**config, x=x)
+        return Priors(prior_func, transform_func, config)
+
+    @classmethod
+    def get_uniform_prior(cls, lower_bound: float, upper_bound: float) -> Self:
+        config = {
+                "lower_bound":lower_bound,
+                "upper_bound": upper_bound,
+        }
+        transform_func = lambda x, config=config: uniform_transform(**config, x=x)
+        prior_func = lambda x, config=config: uniform_density(**config, x=x)
+        return Priors(prior_func, transform_func, config)
+
+    @classmethod
+    def get_dirac_prior(cls, fixed_val: float) -> Self:
+        config = {
+                "fixed_val": fixed_val,
+        }
+        transform_func = lambda x, config=config: dirac_delta_transform(**config, x=x)
+        prior_func = lambda x, config=config: dirac_delta_density(**config, x=x)
+        return Priors(prior_func, transform_func, config)
