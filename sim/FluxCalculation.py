b2865a9493d71e67fbdccfcb7122866a3145d9c5
kaimul19 <kaimulcock19@gmail.com>
2025-02-08 14:31:41 +0100

    Add function to generate flux lightcurve with batman package

--- sim/FluxCalculation.py
+++ sim/FluxCalculation.py
@@ -6,6 +6,49 @@ import matplotlib.pyplot as plt
 from numba import jit
 
 
+def use_batman(planet_params, stellar_params, times):
+    """
+    Calculate the flux values using the batman package.
+
+    Parameters:
+    - planet_params: List of planet parameters
+    - stellar_params: List of stellar parameters
+    - times: Array of time values
+
+    Returns:
+    - flux: Array of flux values
+    
+    """
+    import batman
+    flux = np.zeros(len(times))
+    stellar_radius, stellar_mass, limb_darkening_model, limb_darkening_coefficients = stellar_params
+
+    for planet_param in planet_params:
+        a = planet_param[1] * 1.496e11 / stellar_radius
+        t_0 = planet_param[2]*(planet_param[7] + np.pi/2)/(2*np.pi)
+
+        params = batman.TransitParams()
+        params.t0 = t_0                       #time of inferior conjunction
+        params.per = planet_param[2]                      #orbital period
+        params.rp = planet_param[0]                     #planet radius (in units of stellar radii)
+        params.a = a                   #semi-major axis (in units of stellar radii)
+        params.inc = np.degrees(planet_param[4])                     #orbital inclination (in degrees)
+        params.ecc = planet_param[3]                      #eccentricity
+        params.w = planet_param[5]                       #longitude of periastron (in degrees)
+        params.u = limb_darkening_coefficients                #limb darkening coefficients [u1, u2]
+        params.limb_dark = limb_darkening_model       #limb darkening model
+
+        m = batman.TransitModel(params, times)    #initializes model
+        flux += m.light_curve(params) - 1         #calculates light curve
+        plt.plot(times, m.light_curve(params))
+        plt.show()
+
+    flux += 1
+
+    return flux
+
+
+
 @jit
 def delta_flux_from_mandel_and_agol(
     x: float, y: float, z: float, radius_star: float, eta: float
