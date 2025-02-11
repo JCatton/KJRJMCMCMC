51bbbeadb91fa9dac2645f6ba0d9e184cad27f50
JonteC <106551147+JCatton@users.noreply.github.com.>
2025-02-11 02:08:47 +0000

    Same as previous for post burn-in plot

--- MCMC/mcmc.py
+++ MCMC/mcmc.py
@@ -451,13 +451,19 @@ class MCMC:
         chain = chain[self.burn_in_index :]
         x = np.arange(len(chain))
 
+        true_val_idx = 0
         for body in range(chain.shape[1]):
             for i, name in enumerate(param_names[body]):
+                try:
+                    if np.isnan(name):
+                        continue
+                except TypeError:
+                    pass
                 param_samples = chain[:, body, i]
                 print(
                     f"Estimated {name}: {np.mean(param_samples):.3e}",
                     (
-                        f", true {name}: {true_vals[body, i]}"
+                        f", true {name}: {remaining_true_vals[true_val_idx]:.3e}"
                         if true_vals is not None
                         else None
                     ),
@@ -465,7 +471,7 @@ class MCMC:
                 axs[i, body].plot(x, param_samples, label=name)
                 if true_vals is not None:
                     axs[i, body].hlines(
-                        true_vals[body, i],
+                        remaining_true_vals[true_val_idx],
                         xmin=0,
                         xmax=len(chain),
                         linestyles="--",
@@ -473,6 +479,7 @@ class MCMC:
                     )
                 min_val, max_val = minmax(param_samples)
                 axs[i, body].set_ylabel(f"{name}")
+                true_val_idx += 1
 
         plt.tight_layout()
         plt.savefig(self.data_folder / "chain_post_burn_in_plot_plot.pdf", dpi=500)
