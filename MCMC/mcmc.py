Fixing up GHMC to work using a lower rank covariance matrix to account for non-varying parameters.

MCMC/mcmc.py
@@ -121,6 +121,7 @@ class MCMC:
         self.sim_number = 1
         self.data_folder = build_folder_name(specified_folder_name)
         self.plot_save_folder = self.data_folder / "plots"
+        self.plot_save_folder.mkdir(parents=True, exist_ok=True)
         self.param_names = param_names
         self.initial_parameters = initial_parameters
 
@@ -374,7 +375,7 @@ class MCMC:
                      est_burn_in_end: int=5000,
                      estimate_interval: int=1):
 
-        param_number_sq = self.proposal_std.flatten().shape[0] ** 2
+        param_number_sq = self.proposal_std.flatten().shape[0] ** 3
         mh_iter = param_number_sq - self.iteration_num
         if self.iteration_num <= param_number_sq:
             self.metropolis_hastings(mh_iter)
@@ -385,7 +386,8 @@ class MCMC:
 
             self.estimated_covariance_matrix = self.update_covariance(estimate_interval, prev_iter)
             self.estimated_mean = self.update_mean(estimate_interval, prev_iter)
-        self.prepare_chains_for_new_iters(min(10,num_of_new_iterations - mh_iter))
+
+        self.prepare_chains_for_new_iters(max(10,num_of_new_iterations - mh_iter))
 
         pbar = tqdm(
             initial=1, total=num_of_new_iterations, desc="MCMC Run "
@@ -394,7 +396,8 @@ class MCMC:
         current_position = self.chain[prev_iter]
         current_ln_likelihood = self.likelihood_func(current_position)
 
-        for i in range(num_of_new_iterations):
+        print(f"Pre GHMC {self.acceptance_num=}")
+        for i in range(max(10,num_of_new_iterations - mh_iter)):
             accept, new_likelihood, new_pos = self.do_gaussian_hmc_step(current_ln_likelihood, current_position,
                                                                         self.estimated_covariance_matrix, timestep)
             if accept:
@@ -415,6 +418,7 @@ class MCMC:
             pbar.update(1)
 
         print("\n", self.estimated_covariance_matrix)
+        print(f"Post GHMC {self.acceptance_num=}")
         acceptance_rate = self.acceptance_num / (self.rejection_num + self.acceptance_num)
 
         burn_in = self.determine_burn_in_index()
@@ -427,8 +431,8 @@ class MCMC:
         )
         fig.suptitle("Chains")
         for i in range(self.estimated_covariance_matrix.shape[0]):
-            axs[i].plot(domain, self.chain[:, i])#
-        plt.savefig(self.plot_save_folder + "/chains.png")
+            axs[i].plot(domain, self.chain[:, i])
+        plt.savefig(self.plot_save_folder / "chains.png")
         plt.close()
         fig, axs = plt.subplots(
             nrows=self.chain.shape[1], ncols=1, figsize=(10, 8)
@@ -436,7 +440,7 @@ class MCMC:
         fig.suptitle("Burn-in chains")
         for i in range(self.estimated_covariance_matrix.shape[0]):
             axs[i].plot(domain[:burn_in], self.chain[:burn_in, i])
-        plt.savefig(self.plot_save_folder + "/burn-in_chains.png")
+        plt.savefig(self.plot_save_folder / "burn-in_chains.png")
         plt.close()
 
         fig, axs = plt.subplots(
@@ -445,40 +449,48 @@ class MCMC:
         fig.suptitle("Post-Burn-in chains")
         for i in range(self.estimated_covariance_matrix.shape[0]):
             axs[i].plot(domain[burn_in:], self.chain[burn_in:, i])
-        plt.savefig(self.plot_save_folder + "/post-burn-in_chains.png")
+        plt.savefig(self.plot_save_folder / "post-burn-in_chains.png")
         plt.close()
 
         plt.title("Post-Burn-in Likelihoods")
         plt.plot(domain, self.likelihood_chain)
-        plt.savefig(self.plot_save_folder + "/post-burn-in_likelihood.png")
+        plt.savefig(self.plot_save_folder / "post-burn-in_likelihood.png")
         plt.close()
-        corner(self.chain[burn_in:])
-        plt.savefig(self.plot_save_folder + "/corner.png")
+        corner(self.chain[burn_in:, self.varying_mask])
+        plt.savefig(self.plot_save_folder / "corner.png")
         plt.close()
 
         self.chain_wrap_up()
 
     def update_covariance(self, interval, max_iter):
-        return np.corrcoef(self.chain[self.burn_in_index:max_iter:interval, :], rowvar=0)
+        chain_segment = self.chain[self.burn_in_index:max_iter:interval, :]
+        chain_varying = chain_segment[:, self.varying_mask]
+        cov_varying = np.corrcoef(chain_varying, rowvar=False)
+        return cov_varying
 
     def update_mean(self, interval, max_iter):
-        return np.mean(self.chain[self.burn_in_index:max_iter:interval, :], axis=0)
+        chain_segment = self.chain[self.burn_in_index:max_iter:interval, :]
+        mean_varying = np.mean(chain_segment[:, self.varying_mask], axis=0)
+        return mean_varying
 
     def hamiltonian(self, cov, pos, mean, mom):
-        delta = pos - mean
+        delta = pos[self.varying_mask] - mean
         return -self.likelihood_func(pos) + 0.5 * mom.transpose() @ np.linalg.inv(cov) @ mom
 
     def do_gaussian_hmc_step(self, current_ln_likelihood, current_pos, covariance_mat, timestep):
 
         expected_mean = self.estimated_mean
-        current_normal = multivariate_normal(np.zeros(len(current_pos)), covariance_mat)
+        current_normal = multivariate_normal(np.zeros(len(current_pos[self.varying_mask])), covariance_mat)
 
         current_mom = current_normal.rvs() # Velocity sample
         a_i = np.linalg.inv(covariance_mat) @ current_mom
-        b_i = current_pos - expected_mean
+        b_i = current_pos[self.varying_mask] - expected_mean
 
-        new_pos = expected_mean + a_i * np.sin(timestep) + b_i * np.cos(timestep)
+        new_varying_pos = expected_mean + a_i * np.sin(timestep) + b_i * np.cos(timestep)
         new_mom = covariance_mat @ (a_i * np.cos(timestep) - b_i * np.sin(timestep))
+
+        new_pos = current_pos.copy()
+        new_pos[self.varying_mask] = new_varying_pos
 
         new_ln_likelihood = self.likelihood_func(new_pos)
 