0accd9398a273c2bd08ece6f155c5cef329f5f75
JonteC <106551147+JCatton@users.noreply.github.com.>
2025-03-10 23:54:17 +0000

    Updating nested sampler precision

--- MCMC/mcmc.py
+++ MCMC/mcmc.py
@@ -243,7 +243,7 @@ class MCMC:
         ndim = np.sum(self.varying_mask)
         sampler = dynesty.NestedSampler(loglikelihood=li_fn, prior_transform=prior_transform,
                                         ndim=ndim, nlive=20_000)
-        sampler.run_nested()
+        sampler.run_nested(dlogz=0.00005)
         sresults = sampler.results
         self.nested_results = sresults
         with open(self.data_folder / "nested_sampling_results.pkl", "wb") as f:
